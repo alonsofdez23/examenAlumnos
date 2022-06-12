9091feda324500863db9c3532a24cad75fc59b25
alonsofdez23 <alonsobs23@gmail.com>
2022-06-12 12:53:12 +0200

    Migraciones

---
+++ app/Models/Ccee.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ccee extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ccee';
+}
